f534a29808eaa6c6815bea0fb5dcfc7c88618a0d
yashraj0408 <yashraj7250@gmail.com>
2023-06-11 00:51:03 +0530

    add portfolio button added

--- lib/screens/assets.dart
+++ lib/screens/assets.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:money_mate/components/reusable_card.dart';
+import 'package:money_mate/constants.dart';
 
 class Assets extends StatefulWidget {
   @override
@@ -6,6 +8,8 @@ class Assets extends StatefulWidget {
 }
 
 class _AssetsState extends State<Assets> {
+  List<String> portfolioItems = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +25,96 @@ class _AssetsState extends State<Assets> {
         title: Text('MoneyMate'),
       ),
       body: SafeArea(
-        child: Column(
-          children: [],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    portfolioItems.isEmpty
+                        ? "Seems like you don’t have any portfolio. Create one."
+                        : "Your Portfolio:",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 20),
+                  if (portfolioItems.isEmpty)
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          portfolioItems.add("New Portfolio Item");
+                        });
+                      },
+                      child: Icon(Icons.add),
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(16),
+                        backgroundColor: kPurple,
+                      ),
+                    ),
+                  if (portfolioItems.isNotEmpty)
+                    Column(
+                      children: [
+                        for (String item in portfolioItems)
+                          ReusableCard(
+                            aspectRatio: 4,
+                            colour: kCardColor,
+                            cardChild: ListTile(
+                              title: Text(item),
+                            ),
+                          ),
+                        // Card(
+                        //   child: ListTile(
+                        //     title: Text(item),
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  if (portfolioItems.isNotEmpty)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 17, right: 17),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                portfolioItems.add("New Portfolio Item");
+                              });
+                            },
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.add,
+                                  size: 30,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text("Add portfolio"),
+                              ],
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    15), // Adjust the radius as needed
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 24),
+                              backgroundColor: kPurple,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
       // bottomNavigationBar: BottomNavBar(),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,165 +12,168 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ReusableCard(
-              aspectRatio: 6,
-              colour: kBgColor,
-              cardChild: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(children: [
-                    const CircleAvatar(
-                      radius: 20,
-                      backgroundImage: AssetImage('images/dp.png'),
-                    ),
-                    SizedBox(width: 15),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Hey, Yash",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          "Welcome back to Moneymate",
-                        ),
-                      ],
-                    )
-                  ]),
-                ],
-              ),
-            ),
-            ReusableCard(
-              colour: kCardColor,
-              cardChild: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25, right: 25),
-                    child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ReusableCard(
+                aspectRatio: 6,
+                colour: kBgColor,
+                cardChild: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(children: [
+                      const CircleAvatar(
+                        radius: 20,
+                        backgroundImage: AssetImage('images/dp.png'),
+                      ),
+                      SizedBox(width: 15),
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Total Balance :',
-                            style: TextStyle(
-                              fontSize: 18.0,
-
-                              // color: Colors.black,
-                            ),
-                          ),
-                          SizedBox(height: 6),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                child: Icon(Icons.currency_rupee),
-                                height: 34,
-                              ),
-                              const Text(
-                                '120000',
-                                style: TextStyle(
-                                  fontSize: 38.0,
-                                  fontWeight: FontWeight.w500,
-                                  // color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ]),
-                  )
-                ],
-              ),
-            ),
-            ReusableCard(
-              colour: kCardColor,
-              cardChild: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25, right: 25),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Total Invested :',
-                            style: TextStyle(
-                              fontSize: 18.0,
-
-                              // color: Colors.black,
-                            ),
-                          ),
-                          SizedBox(height: 6),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                child: Icon(Icons.currency_rupee),
-                                height: 34,
-                              ),
-                              const Text(
-                                '100000',
-                                style: TextStyle(
-                                  fontSize: 38.0,
-                                  fontWeight: FontWeight.w500,
-                                  // color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ]),
-                  )
-                ],
-              ),
-            ),
-            ReusableCard(
-              colour: kCardColor,
-              cardChild: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25, right: 25),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+                        children: const [
                           Text(
-                            'Total Profit :',
-                            style: TextStyle(
-                              fontSize: 18.0,
-
-                              // color: Colors.black,
-                            ),
+                            "Hey, Yash",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          SizedBox(height: 6),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                child: Icon(Icons.currency_rupee),
-                                height: 34,
-                              ),
-                              Text(
-                                '20000',
-                                style: TextStyle(
-                                  fontSize: 38.0,
-                                  fontWeight: FontWeight.w500,
-                                  // color: Colors.black,
-                                ),
-                              ),
-                            ],
+                          SizedBox(height: 5),
+                          Text(
+                            "Welcome back to Moneymate",
                           ),
-                        ]),
-                  )
-                ],
+                        ],
+                      )
+                    ]),
+                  ],
+                ),
               ),
-            ),
-          ],
+              ReusableCard(
+                colour: kCardColor,
+                cardChild: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, right: 25),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Total Balance :',
+                              style: TextStyle(
+                                fontSize: 18.0,
+
+                                // color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 6),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: Icon(Icons.currency_rupee),
+                                  height: 34,
+                                ),
+                                const Text(
+                                  '120000',
+                                  style: TextStyle(
+                                    fontSize: 38.0,
+                                    fontWeight: FontWeight.w500,
+                                    // color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                    )
+                  ],
+                ),
+              ),
+              ReusableCard(
+                colour: kCardColor,
+                cardChild: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, right: 25),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Total Invested :',
+                              style: TextStyle(
+                                fontSize: 18.0,
+
+                                // color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 6),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: Icon(Icons.currency_rupee),
+                                  height: 34,
+                                ),
+                                const Text(
+                                  '100000',
+                                  style: TextStyle(
+                                    fontSize: 38.0,
+                                    fontWeight: FontWeight.w500,
+                                    // color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                    )
+                  ],
+                ),
+              ),
+              ReusableCard(
+                colour: kCardColor,
+                cardChild: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, right: 25),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Total Profit :',
+                              style: TextStyle(
+                                fontSize: 18.0,
+
+                                // color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 6),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: Icon(Icons.currency_rupee),
+                                  height: 34,
+                                ),
+                                Text(
+                                  '20000',
+                                  style: TextStyle(
+                                    fontSize: 38.0,
+                                    fontWeight: FontWeight.w500,
+                                    // color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       // bottomNavigationBar: BottomNavBar(),
