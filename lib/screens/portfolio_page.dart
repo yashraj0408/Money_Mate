b4aa0ad852e1c6364e3baa0953b546ac4b38bf2f
yashraj0408 <yashraj7250@gmail.com>
2023-07-02 02:37:39 +0530

    add-asset popup added

--- lib/screens/portfolio_page.dart
+++ lib/screens/portfolio_page.dart
@@ -7,10 +7,34 @@ class PortfolioPage extends StatefulWidget {
   _PortfolioPageState createState() => _PortfolioPageState();
 }
 
+class Asset {
+  final String name;
+  final String symbol;
+
+  Asset({required this.name, required this.symbol});
+}
+
 class _PortfolioPageState extends State<PortfolioPage> {
   String portfolioName = '';
-  List<String> AssetsItems = [];
+  List<Asset> assets = [];
   ScrollController _scrollController = ScrollController();
+  TextEditingController assetNameController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
+  TextEditingController buyingPriceController = TextEditingController();
+  Asset? selectedAsset;
+
+  List<Asset> topAssets = [
+    Asset(name: 'Bitcoin', symbol: 'BTC'),
+    Asset(name: 'Ethereum', symbol: 'ETH'),
+    Asset(name: 'Binance Coin', symbol: 'BNB'),
+    Asset(name: 'Cardano', symbol: 'ADA'),
+    Asset(name: 'XRP', symbol: 'XRP'),
+    Asset(name: 'Dogecoin', symbol: 'DOGE'),
+    Asset(name: 'Polkadot', symbol: 'DOT'),
+    Asset(name: 'Litecoin', symbol: 'LTC'),
+    Asset(name: 'Bitcoin Cash', symbol: 'BCH'),
+    Asset(name: 'Chainlink', symbol: 'LINK'),
+  ];
 
   @override
   void didChangeDependencies() {
@@ -24,13 +48,10 @@ class _PortfolioPageState extends State<PortfolioPage> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        String inputText = '';
-
         return Dialog(
-          backgroundColor: kCardColor, // Set the background color of the dialog
+          backgroundColor: kCardColor,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(
-                10.0), // Apply rounded corners to the dialog
+            borderRadius: BorderRadius.circular(10.0),
           ),
           child: Container(
             padding: const EdgeInsets.all(16.0),
@@ -38,20 +59,45 @@ class _PortfolioPageState extends State<PortfolioPage> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 const Text(
-                  'Add Assets',
+                  'Add Asset',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 16.0),
-                TextField(
-                  onChanged: (value) {
-                    inputText = value;
+                DropdownButtonFormField<Asset>(
+                  value: selectedAsset,
+                  onChanged: (asset) {
+                    setState(() {
+                      selectedAsset = asset;
+                    });
                   },
+                  dropdownColor: kCardColor,
+                  items: topAssets.map((Asset asset) {
+                    return DropdownMenuItem<Asset>(
+                      value: asset,
+                      child: Text('${asset.name} (${asset.symbol})'),
+                    );
+                  }).toList(),
                   decoration: kInputTextDecoration.copyWith(
-                    hintText: '',
                     labelText: 'Asset Name',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: amountController,
+                  decoration: kInputTextDecoration.copyWith(
+                    labelText: 'Amount',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: buyingPriceController,
+                  decoration: kInputTextDecoration.copyWith(
+                    labelText: 'Buying Price',
                     labelStyle: TextStyle(color: Colors.white),
                   ),
                 ),
@@ -71,11 +117,17 @@ class _PortfolioPageState extends State<PortfolioPage> {
                     TextButton(
                       onPressed: () {
                         setState(() {
-                          AssetsItems.add(inputText);
+                          String assetName = selectedAsset?.name ?? '';
+                          String symbol = selectedAsset?.symbol ?? '';
+                          String amount = amountController.text;
+                          String buyingPrice = buyingPriceController.text;
+                          assets.add(Asset(name: assetName, symbol: symbol));
+                          amountController.clear();
+                          buyingPriceController.clear();
+                          selectedAsset = null;
                         });
                         Navigator.of(context).pop();
-                        //scroll to the end
-                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                        WidgetsBinding.instance!.addPostFrameCallback((_) {
                           _scrollController.animateTo(
                             _scrollController.position.maxScrollExtent,
                             duration: const Duration(milliseconds: 300),
@@ -84,7 +136,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
                         });
                       },
                       child: const Text(
-                        'Create',
+                        'Add',
                         style: TextStyle(color: kLightBlue),
                       ),
                     ),
@@ -102,7 +154,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(portfolioName),
+        title: Text('$portfolioName Portfolio'),
       ),
       body: SafeArea(
         child: Center(
@@ -116,8 +168,8 @@ class _PortfolioPageState extends State<PortfolioPage> {
                   Padding(
                     padding: const EdgeInsets.only(left: 20, right: 20),
                     child: Text(
-                      AssetsItems.isEmpty
-                          ? "Seems like you don’t have any asset in $portfolioName Portfolio.\nAdd one.\n👇"
+                      assets.isEmpty
+                          ? "Seems like you don’t have any assets in $portfolioName Portfolio.\nAdd one.\n👇"
                           : "Your Assets:",
                       style: const TextStyle(
                         fontSize: 18,
@@ -127,7 +179,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
                     ),
                   ),
                   SizedBox(height: 20),
-                  if (AssetsItems.isEmpty)
+                  if (assets.isEmpty)
                     ElevatedButton(
                       onPressed: _showInputDialog,
                       child: Icon(Icons.add),
@@ -137,26 +189,35 @@ class _PortfolioPageState extends State<PortfolioPage> {
                         backgroundColor: kPurple,
                       ),
                     ),
-                  if (AssetsItems.isNotEmpty)
+                  if (assets.isNotEmpty)
                     Column(
                       children: [
-                        for (String item in AssetsItems)
+                        for (Asset asset in assets)
                           ReusableCard(
                             aspectRatio: 4,
                             colour: kCardColor,
                             cardChild: ListTile(
                               title: Padding(
                                 padding: const EdgeInsets.only(
-                                    left: 10, right: 10, top: 0, bottom: 10),
+                                    left: 0, right: 4, top: 2, bottom: 2),
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    Text(
-                                      item,
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 30,
+                                    ListTile(
+                                      title: Text(
+                                        asset.symbol,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 27,
+                                        ),
+                                      ),
+                                      subtitle: Text(
+                                        asset.name,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 16,
+                                        ),
                                       ),
                                     ),
                                   ],
@@ -169,7 +230,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
                   SizedBox(
                     height: 15,
                   ),
-                  if (AssetsItems.isNotEmpty)
+                  if (assets.isNotEmpty)
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
